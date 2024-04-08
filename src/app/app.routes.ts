d1ab9e252ab2c9dd22ef415458c2499a772e603c
caiolima <caiolima@fieg.com.br>
2024-04-08 09:28:08 -0300

    foda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-
-export const routes: Routes = [];
-=======
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageMercadosComponent } from './pages/page-mercados/page-mercados.component';
 import { ProdutosPageComponent } from './pages/produtos-page/produtos-page.component';
@@ -12,6 +8,5 @@ export const routes: Routes = [
     { path: '', component: HomePageComponent, title: 'Home' },
     { path: 'mercados-page', component: PageMercadosComponent, title: 'Mercados' },
     { path: 'produtos-page', component: ProdutosPageComponent, title: 'Produtos' },
-    { path: 'item-pagina', component: ItemPaginaComponent, title: 'Item'}    
+    { path: 'item-pagina', component: ItemPaginaComponent, title: 'Item' }
 ];
->>>>>>> 7b8861dddb473a04b9ec41452cac483d4de312b8
